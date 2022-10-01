c46ddf6e57bd818b14b7190d5ecedaac1dd1ad66
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2022-10-01 18:58:29 -0400

    Project Spacing/Styling

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -7,7 +7,7 @@ function Projects(props) {
     const projectImages = props.projectImage.map(image => {
         return (
             <>
-                <Grid item xs={12} md={4} key={image.id}>
+                <Grid item xs={12} sm={6} md={4} key={image.id}>
                     <a href={image.url} target="_blank" rel="noreferrer">
                         <img className="project-image" alt={image.text} src={image.image}></img>
                     </a>
@@ -19,7 +19,7 @@ function Projects(props) {
     return (
         <>
             <div className='projectContainer'>
-                <Grid container spacing={3}>
+                <Grid container spacing={2}>
                     {projectImages}
                 </Grid>
             </div>
