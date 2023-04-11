c169cd4e7b243dfeb1eb7511971358ba3ef5e87b
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-04-10 20:35:12 -0400

    migrated from class to functional component, migrated MainComponent to App.js, deleted MainComponent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import Main from './components/MainComponent';
-import { BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
+import Header from './components/HeaderComponent';
+import Home from './components/HomeComponent';
+import Projects from './components/ProjectComponent';
+import About from './components/AboutComponent';
+import Contact from './components/ContactComponent';
+import Footer from './components/FooterComponent';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Skills_Data } from './shared/Product_Data';
+import { Project_Image } from './shared/Product_Data';
 import './styles/App.scss';
 
 
-class App extends Component {
+const App = () => {
 
-  
-  render() {
-      return (  
-        <BrowserRouter>
-          <div className="App">
-              <Main /> 
-          </div>
-        </BrowserRouter>
-      );
-  }
+  const [skillsData, setSkillsData] = useState(Skills_Data);
+  const [projectImg, setProjectImg] = useState(Project_Image);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path='/home' render={() => <Home skills={skillsData} />} />
+          <Route path='/projects' render={() => <Projects projectImage={projectImg} />} />
+          <Route path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Redirect to='/home' />
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+
 }
 
-export default App;
+export default App; 
