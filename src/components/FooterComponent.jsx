3070483d8bb51a53cc52b195d179f1a8c97f86d1
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-09-06 20:20:27 -0400

    spacing & styling adjustments

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
     }
 
     return (
-        <>
+        <div className="container mx-auto">
             <div className="text-center mt-10 pb-10 lg:flex lg:p-7 lg:items-center lg:gap-x-10">
-                <div className="flex justify-center gap-x-5">
+                <div className="flex justify-center gap-x-5 lg:order-last">
                     <a href="https://twitter.com/Renz_757" target="_blank" rel="noreferrer"><TwitterIcon className="w-10 h-10 text-accent-color" fontSize="medium" /></a>
                     <a href="https://github.com/Renz757" target="_blank" rel="noreferrer"><GitHubIcon className="w-10 h-10 text-accent-color" fontSize="medium" /></a>
                     <a href="https://www.linkedin.com/in/lorenzo-tinsley" target="_blank" rel="noreferrer"><LinkedInIcon className="w-10 h-10 text-accent-color" fontSize="medium" /></a>
@@ -28,7 +28,7 @@ const Footer = () => {
                     <li onClick={toTop}><Link className="no-underline hover:text-accent-color lg:text-xl" to="contact">Contact</Link></li>
                 </ul>
             </div>
-        </>
+        </div>
     );
 }
 
